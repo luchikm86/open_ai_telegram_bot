e560640a169698c565f222feba61e79890d15635
luchikm <luchik.m86@gmail.com>
2025-12-16 22:25:22 +0200

    Modified files  .env.example,  src/config.py. Add bot token

--- src/config.py
+++ src/config.py
@@ -5,5 +5,10 @@ from dotenv import load_dotenv
 # Завантажуємо змінні з .env файлу
 load_dotenv()
 
-# Отримуємо токен з .env файлу
+# Отримуємо токени з .env файлу
 CHATGPT_TOKEN = os.getenv("CHATGPT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Виводимо токени в консоль (Видалити після перевірки)
+print(CHATGPT_TOKEN)
+print(BOT_TOKEN)
